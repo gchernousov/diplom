8ce8528a4534cb4c4d8515d210f2ee1f0763c9b0
gchernousov <seasidesuicide@yandex.ru>
2023-02-28 21:58:13 +0500

    add test account login/token

--- tests/api_backend/test_api.py
+++ tests/api_backend/test_api.py
@@ -1,8 +1,10 @@
 import pytest
 from rest_framework.test import APIClient
+from model_bakery import baker
 
 from api_backend.models import UserModel
 from api_backend.serializers import UserSerializer
+from api_backend.validation import check_login
 
 
 URL = 'http://127.0.0.1:8000/'
@@ -15,34 +17,57 @@ def client():
 
 # @pytest.fixture
 # def add_user():
-#     user = UserModel(email='testuser@testmail.com', password='pG6bc4Xo9Ms')
-#     return user
+#     data = {'email': 'testuser@testmail.com', 'password': 'hT3x*2NSdP9bA'}
+#     user_serializer = UserSerializer(data=data)
+#     if user_serializer.is_valid():
+#         user = user_serializer.save()
+#         user.set_password(data['password'])
+#         user.save()
+#
+#         return user
+#     return False
+
+
+
+def new_user(email, password):
+    user = UserModel(email=email, password=password)
+    user.set_password(password)
+    user.save()
+    return user
+
+
+# @pytest.fixture
+# def add_new_user():
+#     def factory(*args, **kwargs):
+#         return baker.make(UserModel, *args, **kwargs)
+#     return factory
+
 
 # def get_object(model, object_id):
 #     object = model.objects.get(pk=object_id)
 #     return object
 
+
 @pytest.mark.django_db
 def test_account_register(client):
+    print('>>> test_account_register')
     data = {'email': 'mrsmith@mail.com', 'password': 'Jn9c2Xy6Qm*1'}
     response = client.post(f'{URL}api/v1/user/register/', data=data, format='json')
     assert response.status_code == 200
     new_user = UserModel.objects.latest('pk')
+    print(f'new_user = {new_user}')
     assert new_user.email == 'mrsmith@mail.com'
 
 
 @pytest.mark.django_db
 def test_account_login(client):
-    user = UserModel(email='testuser@testmail.com', password='pG6bc*4Xo9Ms')
-    user.save()
-    print(f'user = {user}')
-    data = {'email': user.email, 'password': user.password}
-    print(f'data = {data}')
+    print('>>> test_account_login')
+    data = {'email': 'testuser@yandex.ru', 'password': 'Xg5ca9Lw2*k'}
+    user = new_user(data['email'], data['password'])
+    print(f'user.email = {user.email}, user.password = {user.password}')
     response = client.post(f'{URL}api/v1/user/login/', data=data, format='json')
-    print(f'response = {response}')
     assert response.status_code == 200
-    print(f'response.json() = {response.json()}')
     response_data = response.json()
-    users = UserModel.objects.all()
-    print(f'users = {users}')
-    assert response_data['Token'] == 'uiduihja'
+    print(f'response_data = {response_data}')
+    result = 'Token' in response_data.keys()
+    assert result == True
