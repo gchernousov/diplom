e86c6421fca17b5aec12150bfc88479566c3d818
gchernousov <seasidesuicide@yandex.ru>
2023-03-27 19:53:27 +0500

    fix aithorization, add tests

--- api_backend/validation.py
+++ api_backend/validation.py
@@ -11,9 +11,9 @@ class APIError(APIException):
     default_detail = ''
 
 
-class IsOwner(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+# class IsOwner(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         return obj.user == request.user
 
 
 def get_object(model, object_id):
@@ -45,13 +45,13 @@ def check_email(email: str) -> bool:
         return True
 
 
-def check_token(token):
-    """Проверка наличия токена в базе"""
-    try:
-        token = Token.objects.get(key=token)
-        return token
-    except ObjectDoesNotExist:
-        return None
+# def check_token(token):
+#     """Проверка наличия токена в базе"""
+#     try:
+#         token = Token.objects.get(key=token)
+#         return token
+#     except ObjectDoesNotExist:
+#         return None
 
 
 def check_shop(user: object, shop_name: str) -> bool:
@@ -63,16 +63,3 @@ def check_shop(user: object, shop_name: str) -> bool:
         return False
     except ObjectDoesNotExist:
         return True
-
-
-def auth_check(request):
-    """Проверка наличия токена в заголовке, его наличия в базе
-    и возврат объекта пользователя, если все успешно"""
-
-    if request.headers.get('Token') is None:
-        raise APIError('Не указан Token. Вы не авторизованы.')
-    token = check_token(request.headers['Token'])
-    if token is None:
-        raise APIError('Token неверный.')
-    user = UserModel.objects.get(email=token.user)
-    return user
